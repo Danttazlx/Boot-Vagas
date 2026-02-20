afc2ea18759d494b28b256c6660339a9109ef824
daniel <danttaz11@gmail.com>
2026-02-20 19:35:40 -0300

    limpando as importacoes do repositry

--- src/main/java/com/VagasBoot/Boot/Vagas/repository/RepositoryVaga.java
+++ src/main/java/com/VagasBoot/Boot/Vagas/repository/RepositoryVaga.java
@@ -3,8 +3,6 @@ package com.VagasBoot.Boot.Vagas.repository;
 import com.VagasBoot.Boot.Vagas.model.Vaga;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface RepositoryVaga extends JpaRepository< Vaga,Long> {
 
 
