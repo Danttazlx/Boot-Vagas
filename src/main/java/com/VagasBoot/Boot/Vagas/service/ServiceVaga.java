cb6581f731a18cc17e0263db1326eee6ce1c13be
daniel <danttaz11@gmail.com>
2026-02-22 02:01:27 -0300

    commit seguranca service

--- src/main/java/com/VagasBoot/Boot/Vagas/service/ServiceVaga.java
+++ src/main/java/com/VagasBoot/Boot/Vagas/service/ServiceVaga.java
@@ -1,4 +1,26 @@
 package com.VagasBoot.Boot.Vagas.service;
 
+import com.VagasBoot.Boot.Vagas.dto.VagaDTO;
+import com.VagasBoot.Boot.Vagas.repository.RepositoryVaga;
+
 public class ServiceVaga {
+
+    private final RepositoryVaga repositoryVaga;
+
+    public ServiceVaga(RepositoryVaga repositoryVaga) {
+        this.repositoryVaga = repositoryVaga;
+    }
+
+        public void processarVaga (VagaDTO dtoVaga){
+
+        if (dtoVaga.getLink() == null && dtoVaga.getLink().isBlank()){
+              throw new IllegalArgumentException("Link vazio");
+
+            }
+
+
+
+
+    }
+
 }
