60bacc880fe1231c271481ef368090d789f9f3ca
daniel <danttaz11@gmail.com>
2026-02-25 01:21:32 -0300

    validando o dto, validando se o link existe no banco, filtro por vagas, e salvando no banco.

--- src/main/java/com/VagasBoot/Boot/Vagas/service/ServiceVaga.java
+++ src/main/java/com/VagasBoot/Boot/Vagas/service/ServiceVaga.java
@@ -1,7 +1,10 @@
 package com.VagasBoot.Boot.Vagas.service;
 
 import com.VagasBoot.Boot.Vagas.dto.VagaDTO;
+import com.VagasBoot.Boot.Vagas.model.Vaga;
 import com.VagasBoot.Boot.Vagas.repository.RepositoryVaga;
+
+import java.util.List;
 
 public class ServiceVaga {
 
@@ -11,16 +14,60 @@ public class ServiceVaga {
         this.repositoryVaga = repositoryVaga;
     }
 
-        public void processarVaga (VagaDTO dtoVaga){
-
-        if (dtoVaga.getLink() == null || dtoVaga.getLink().isBlank()){
-              throw new IllegalArgumentException("Link vazio");
-
-            }
+    public void processarVaga(VagaDTO dtoVaga) {
 
 
+        if (dtoVaga.getLink() == null || dtoVaga.getLink().isBlank()) {
+            throw new IllegalArgumentException("Link vazio");
+        }
 
+        if (repositoryVaga.existsByLink(dtoVaga.getLink())) {
+            System.out.println("link existente no banco!");
+            return;
+        }
+
+        Vaga vaga = converterDto(dtoVaga);
+        repositoryVaga.save(vaga);
 
     }
+    public Vaga converterDto(VagaDTO dto) {
+
+        Vaga vaga = new Vaga();
+
+        vaga.setTituloVaga(dto.getTitulo());
+        vaga.setDescricao(dto.getDescricao());
+        vaga.setEmpresa(dto.getEmpresa());
+        vaga.setLocalizacao(dto.getLocalizacao());
+        vaga.setDataCriacao(dto.getDataPublicacao());
+        vaga.setLink(dto.getLink());
+
+        String tituloVaga = vaga.getTituloVaga().toLowerCase().trim();
+
+        List<String> filtro = List.of(
+                "estagio",
+                "estagio java",
+                "estagio ti",
+                "suporte n1",
+                "estagio desenvolvimento",
+                "suporte tecnico n1",
+                "suporte tecnico n2",
+                "estagio desenvolvimento de software java",
+                "suporte",
+                "n1",
+                "help desk",
+                "suporte tecnico",
+                "estagio desenvolvimento java");
+
+
+        for (String lista : filtro) {
+            if (tituloVaga.contains(lista)) {
+                vaga.setTituloVaga(tituloVaga);
+                break;
+            }
+        }
+        return vaga;
+
+    }
+
 
 }
