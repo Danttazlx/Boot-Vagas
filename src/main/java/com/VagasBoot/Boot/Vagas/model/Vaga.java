8759aeeef6bd2ba0b1a1df444f2b612d40a18114
daniel <danttaz11@gmail.com>
2026-02-26 17:46:31 -0300

    ajustando a entidade vaga

--- src/main/java/com/VagasBoot/Boot/Vagas/model/Vaga.java
+++ src/main/java/com/VagasBoot/Boot/Vagas/model/Vaga.java
@@ -1,45 +1,45 @@
-package com.VagasBoot.Boot.Vagas.model;
+    package com.VagasBoot.Boot.Vagas.model;
 
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+    import jakarta.persistence.*;
+    import lombok.Getter;
+    import lombok.NoArgsConstructor;
+    import lombok.Setter;
 
-import java.time.LocalDateTime;
+    import java.time.LocalDateTime;
 
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@Table(name = "vaga_tech")
-public class Vaga {
+    @Entity
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @Table(name = "vaga")
+    public class Vaga {
 
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    @Column(name = "titulo_vaga")
-    private String tituloVaga;
+        @Column(name = "titulo_vaga",nullable = false)
+        private String tituloVaga;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false, length = 20)
-    private String status;
+        @Enumerated(EnumType.STRING)
+        @Column(name = "status", nullable = false, length = 20)
+        private TipoStatus status;
 
-    @Column(name = "descricao", nullable = false)
-    private String descricao;
+        @Column(name = "descricao", nullable = false)
+        private String descricao;
 
-    @Column(name = "link", nullable = false)
-    private String link;
+        @Column(name = "link", nullable = false, unique = true)
+        private String link;
 
-    @Column(name = "data_criacao", nullable = false)
-    private LocalDateTime dataCriacao;
+        @Column(name = "data_criacao", nullable = false)
+        private LocalDateTime dataCriacao;
 
-    @Column(name = "localizacao_vaga", nullable = false)
-    private String Localizacao;
+        @Column(name = "localizacao_vaga", nullable = false)
+        private String localizacao;
 
-    @Column(name = "empresa", nullable = false)
-    private String empresa;
+        @Column(name = "empresa", nullable = false)
+        private String empresa;
 
-}
+    }
