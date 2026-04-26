c459a1aa257fc27bd8e944228d9ae6256db6c1ad
Daniel <Danttaz11@gmail.com>
2026-04-26 13:06:45 -0300

    refactor: muda os campos do VagaDTO

--- src/main/java/com/VagasBoot/Boot/Vagas/dto/VagaDTO.java
+++ src/main/java/com/VagasBoot/Boot/Vagas/dto/VagaDTO.java
@@ -2,25 +2,17 @@ package com.VagasBoot.Boot.Vagas.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-
 @Getter
 @Setter
 public class VagaDTO {
 
-    private String descricao;
-
-    private String tipo;
-
-    private String area;
-
-    private String localizacao;
-
-    private String salario;
-
-    private String linkcompleto;
-
-    private String expediente;
-
-
+    private String name;
+    private String description;
+    private String type;
+    private String state;
+    private String workplaceType;
 
 }
+
+
+
